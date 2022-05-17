966d6ad89668ed41af5eb49fe879b738eedaaa76
Pillifino <dinodizon073100@gmail.com>
2022-05-17 09:22:09 -0700

    Added api routes with restful crud operations, still missing update routes however

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+// use object destructuring to import our two models by name
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -6,15 +7,47 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No product found With that id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    // Since the model will create a unique UUID value by default, 
+    // we just need to provide the `id` of the Category that will own this Product
+    const locationData = await Category.create({
+      category_id: req.body.category_id, // Foreigh Key in Product model that connects to Category model's id
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
@@ -23,6 +56,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No product card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
